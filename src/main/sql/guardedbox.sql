d4ab5d39192f8b5492a94ae71e55235cf44a5faf
juanjosetorres@kpmg.es <s3curitybug@gmail.com>
2020-03-02 00:46:04 +0100

    SQL schema updated: groups, uuid pk, char vs varchar

--- src/main/sql/guardedbox.sql
+++ src/main/sql/guardedbox.sql
@@ -20,13 +20,65 @@ DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
-  `account_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_id` binary(16) NOT NULL,
   `email` varchar(254) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `salt` varchar(44) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `encryption_public_key` varchar(44) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `signing_public_key` varchar(44) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `salt` char(44) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `encryption_public_key` char(44) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `signing_public_key` char(44) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`account_id`),
   KEY `EMAIL` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `group_`
+--
+
+DROP TABLE IF EXISTS `group_`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group_` (
+  `group_id` binary(16) NOT NULL,
+  `owner_account_id` binary(16) DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `encrypted_group_key` char(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`group_id`),
+  KEY `OWNER_ACCOUNT_ID` (`owner_account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `group_participant`
+--
+
+DROP TABLE IF EXISTS `group_participant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group_participant` (
+  `group_participant_id` binary(16) NOT NULL,
+  `group_id` binary(16) DEFAULT NULL,
+  `account_id` binary(16) DEFAULT NULL,
+  `encrypted_group_key` char(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`group_participant_id`),
+  KEY `GROUP_ID` (`group_id`),
+  KEY `ACCOUNT_ID` (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `group_secret`
+--
+
+DROP TABLE IF EXISTS `group_secret`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group_secret` (
+  `group_secret_id` binary(16) NOT NULL,
+  `group_id` binary(16) DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `value` varchar(16016) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`group_secret_id`),
+  KEY `GROUP_ID` (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -38,9 +90,9 @@ DROP TABLE IF EXISTS `registration`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `registration` (
-  `registration_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `registration_id` binary(16) NOT NULL,
   `email` varchar(254) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `token` varchar(86) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `token` char(86) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `expedition_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`registration_id`),
   KEY `EMAIL` (`email`),
@@ -56,8 +108,8 @@ DROP TABLE IF EXISTS `secret`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `secret` (
-  `secret_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `owner_account_id` bigint(20) DEFAULT NULL,
+  `secret_id` binary(16) NOT NULL,
+  `owner_account_id` binary(16) DEFAULT NULL,
   `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `value` varchar(16016) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`secret_id`),
@@ -66,31 +118,13 @@ CREATE TABLE `secret` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `shared_secret`
+-- Table structure for table `session`
 --
 
-DROP TABLE IF EXISTS `shared_secret`;
+DROP TABLE IF EXISTS `session`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `shared_secret` (
-  `shared_secret_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `secret_id` bigint(20) DEFAULT NULL,
-  `receiver_account_id` bigint(20) DEFAULT NULL,
-  `value` varchar(16016) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`shared_secret_id`),
-  KEY `SECRET_ID` (`secret_id`),
-  KEY `RECEIVER_ACCOUNT_ID` (`receiver_account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `spring_session`
---
-
-DROP TABLE IF EXISTS `spring_session`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `spring_session` (
+CREATE TABLE `session` (
   `PRIMARY_ID` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `SESSION_ID` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `CREATION_TIME` bigint(20) NOT NULL,
@@ -106,18 +140,36 @@ CREATE TABLE `spring_session` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `spring_session_attributes`
+-- Table structure for table `session_attributes`
 --
 
-DROP TABLE IF EXISTS `spring_session_attributes`;
+DROP TABLE IF EXISTS `session_attributes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `spring_session_attributes` (
+CREATE TABLE `session_attributes` (
   `SESSION_PRIMARY_ID` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ATTRIBUTE_NAME` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ATTRIBUTE_BYTES` blob NOT NULL,
   PRIMARY KEY (`SESSION_PRIMARY_ID`,`ATTRIBUTE_NAME`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `shared_secret`
+--
+
+DROP TABLE IF EXISTS `shared_secret`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shared_secret` (
+  `shared_secret_id` binary(16) NOT NULL,
+  `secret_id` binary(16) DEFAULT NULL,
+  `receiver_account_id` binary(16) DEFAULT NULL,
+  `value` varchar(16016) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`shared_secret_id`),
+  KEY `SECRET_ID` (`secret_id`),
+  KEY `RECEIVER_ACCOUNT_ID` (`receiver_account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
